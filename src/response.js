656e31409fdd5027e450f472512153d968843c7f
Sophy Hok <shok@shok-ltmwg2p.internal.salesforce.com>
2022-06-02 21:54:55 -0700

    fixed response data to avoid test fail

--- src/response.js
+++ src/response.js
@@ -1,12 +1,12 @@
 export const response = {
     "list": [
-        { "rank": "1", "points": "1025", "name": "John Doe", "age": "27" },
+        { "rank": "1", "points": "1025", "name": "John Doe", "age": "72" },
         { "rank": "3", "points": "245", "name": "Elizabeth", "age": "17" },
         { "rank": "2", "points": "566", "name": "Samantha", "age": "22" },
         { "rank": "10", "points": "878", "name": "Katherine", "age": "21" },
         { "rank": "11", "points": "778", "name": "Danerys", "age": "25" },
         { "rank": "12", "points": "983", "name": "Eva", "age": "23" },
-        { "rank": "13", "points": "122", "name": "Ruby", "age": "22" },
+        { "rank": "13", "points": "122", "name": "Ruby", "age": "99" },
         { "rank": "14", "points": "656", "name": "Valentina", "age": "27" }, 
         { "rank": "15", "points": "298", "name": "Mila", "age": "32" }, 
         { "rank": "4", "points": "497", "name": "Arya", "age": "31" }, 
